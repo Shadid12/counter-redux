b6010e602b3e2571eaf6409abb45a5b756933e80
shadid12 <shadidhaque2012@gmail.com>
2018-06-26 21:10:29 -0400

    ready to deploy

--- src/containers/sillycounter.js
+++ src/containers/sillycounter.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+
+//css :)
+import './silly.css'
 
 
 // actions 
 import {counterIncrement, counterDecrement} from '../actions/index'
 
 class SillyCounter extends React.Component {
-
+    
     increment = () => {
         let counter = this.props.counter + 1
         this.props.counterIncrement(counter)
@@ -21,9 +27,13 @@ class SillyCounter extends React.Component {
     render() {
         return(
             <div>
-                <h1>{this.props.counter}</h1>
-                <button onClick={this.increment}>+</button>
-                <button onClick={this.decrement}>-</button>
+                <Card className={`card`}>
+                    <CardContent>
+                        <h1>{this.props.counter}</h1>
+                    </CardContent>
+                </Card>
+                <Button variant="outlined" color="primary" onClick={this.increment}>+</Button>
+                <Button variant="outlined" color="secondary" onClick={this.decrement}>-</Button>
             </div>
         )
     }
